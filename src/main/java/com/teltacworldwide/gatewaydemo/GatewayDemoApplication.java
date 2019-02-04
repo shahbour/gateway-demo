07bd6ada1df40bb1f8b8139fd74b43a32c3c4202
Ali Shahbour <shahbour@gmail.com>
2019-02-04 16:40:15 +0200

    remove security

--- src/main/java/com/teltacworldwide/gatewaydemo/GatewayDemoApplication.java
+++ src/main/java/com/teltacworldwide/gatewaydemo/GatewayDemoApplication.java
@@ -3,11 +3,11 @@ package com.teltacworldwide.gatewaydemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.security.oauth2.gateway.TokenRelayGatewayFilterFactory;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
-import org.springframework.security.oauth2.core.user.OAuth2User;
+//import org.springframework.cloud.security.oauth2.gateway.TokenRelayGatewayFilterFactory;
+//import org.springframework.security.core.annotation.AuthenticationPrincipal;
+//import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+//import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+//import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +16,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GatewayDemoApplication {
 
-    @Autowired
-    private TokenRelayGatewayFilterFactory filterFactory;
+//    @Autowired
+//    private TokenRelayGatewayFilterFactory filterFactory;
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayDemoApplication.class, args);
@@ -32,15 +32,15 @@ public class GatewayDemoApplication {
 //                .build();
 //    }
 
-    @GetMapping("/")
-    public String index(Model model,
-                        @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
-                        @AuthenticationPrincipal OAuth2User oauth2User) {
-        model.addAttribute("userName", oauth2User.getName());
-        model.addAttribute("clientName", authorizedClient.getClientRegistration().getClientName());
-        model.addAttribute("userAttributes", oauth2User.getAttributes());
-        return "index";
-    }
+//    @GetMapping("/")
+//    public String index(Model model,
+//                        @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
+//                        @AuthenticationPrincipal OAuth2User oauth2User) {
+//        model.addAttribute("userName", oauth2User.getName());
+//        model.addAttribute("clientName", authorizedClient.getClientRegistration().getClientName());
+//        model.addAttribute("userAttributes", oauth2User.getAttributes());
+//        return "index";
+//    }
 
 }
 
